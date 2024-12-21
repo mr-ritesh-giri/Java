4b4f7a8a6dc483c28f855ca9848f5251a5b69c94
Ritesh Giri <rrgiri032@gmail.com>
2024-12-21 13:05:07 +0530

    Intelij Installation

--- 1-Java Instalation/src/App.java
+++ 1-Java Instalation/src/App.java
@@ -1,10 +1,10 @@
 class Hello{
-    public static void main(String a[]){
+    public static void main(String[] a){
         // System.out.println("Hello World");
         int num1 = 3;
         int num2 = 10;
         int result = num1 + num2;
-       
+
         System.out.println(result);
     }
 }
