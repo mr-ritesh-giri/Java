4b5c253d68d45a2a1592ad997d70c1426363fec2
Ritesh Giri <rrgiri032@gmail.com>
2024-12-23 21:29:08 +0530

    Java Data Types Bit Breakdown

---
+++ 1-Java Instalation/src/AssignmentsDataType.java
@@ -0,0 +1,19 @@
+public class AssignmentsDataType {
+    public static void main(String[] args) {
+        String fullName = "Ritesh Giri";
+        byte age = 20;
+        float height = 1.79f;
+        char favouriteLetter = 'T';
+
+        System.out.println("Name: " + fullName + ", Age: " + age + ", Height: " + height + "m, Favourite Letter: " + favouriteLetter);
+
+        int num = 100;
+        byte b = (byte) num;
+        short s = (short) num;
+        float f = (float) num;
+        double d = num;
+
+
+        System.out.println("Integer: " + num + ", Byte: " + b + ", Short: " + s + ", Float: " + f + ", Double: " + d);
+    }
+}
